86e11a225ac03a821a7ee76125d9afbe7f551158
biezhi <biezhi.me@gmail.com>
2015-12-15 17:05:05 +0800

    -

---
+++ src/main/java/com/precious/controller/admin/OptionsController.java
@@ -0,0 +1,15 @@
+package com.precious.controller.admin;
+
+import com.blade.web.http.Request;
+import com.blade.web.http.Response;
+import com.precious.controller.BaseController;
+
+public class OptionsController extends BaseController {
+	
+	public void home(Request request, Response response){
+		
+	}
+	
+	
+	
+}
