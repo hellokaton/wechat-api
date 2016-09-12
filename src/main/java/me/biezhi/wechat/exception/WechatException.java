21915c58eec1a76c7a4087a97c7c20dfa93a23c3
biezhi <biezhi.me@gmail.com>
2016-09-12 15:33:25 +0800

    ✄ update exception to jdk1.6

--- src/main/java/me/biezhi/wechat/exception/WechatException.java
+++ src/main/java/me/biezhi/wechat/exception/WechatException.java
@@ -7,14 +7,6 @@ public class WechatException extends RuntimeException {
 	public WechatException() {
 		super();
 	}
-
-	public WechatException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-	}
-
-	public WechatException(String message, Throwable cause) {
-		super(message, cause);
-	}
 	
 	public WechatException(String message) {
 		super(message);
